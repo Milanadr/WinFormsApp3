3509abb28eff70527a8f2a09019f3c36b6c51f46
student <student@K29-USER11>
2023-03-24 10:53:08 +0200

    Add project files.

---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            private void radioButton2_CheckedChanged(object sender, EventArgs e)
+            {
+
+                label2.Text = "-" // змінюємо напрямок стрілки textBox 3.Enabled = false; // блокуємо поле textBox3 textBox 4.Enabled = true; // активуємо поле textBox4
+
+
+
+            private void button1_Click(object sender, EventArgs e)// обчислити {
+
+double kurs1, kurs2, suma, sum_out; kurs1 = double.Parse(textBox1.Text); kurs2 = double.Parse(textBox2.Text); if (radioButton1.Checked - tre)
+
+                textBox4.Enabled = true;
+
+            suma = double.Parse(textBox3.Text); sum_out - suma * kursi; textBox4.Text = sum_out.ToString();
+
+els
+    }
+}
